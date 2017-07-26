b3891f615c6a5e8bfd3df2559cb639308f70eb44
roman <ainors@mail.ru>
2017-07-26 14:32:24 +0500

    Add authentication

--- parse/views.py
+++ parse/views.py
@@ -1,25 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
 from .utils import Parse
 from .models import Order
 
 
+@login_required()
 def index(request):
     return render(request, 'parse/index.html')
 
 
+@login_required()
 def load(request, source):
     result = Parse.start_parse(source)
     Order.save_result(result, source)
     return HttpResponseRedirect(reverse('parse:index', args=()))
 
 
+@login_required()
 def show_result(request, source):
     data = Order.objects.filter(source=source)
     return render(request, 'parse/detail.html', {'data': data})
 
 
+@login_required()
 def sort_view(request):
     data = Order.objects.all().order_by('-time_place')
     return render(request, 'parse/detail.html', {'data': data})
